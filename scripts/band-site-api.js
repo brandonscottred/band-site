7d41356b607f4740eb5f827d4b7f762638616d85
Brandon <brandon.redford@outlook.com>
2024-02-28 06:23:43 -0800

    add getComment, getShows and postComment methods to band-site-api.js page

--- scripts/band-site-api.js
+++ scripts/band-site-api.js
@@ -1,6 +1,9 @@
-const baseUrl = 'https://project-1-api.herokuapp.com';
+const apiKey = '2fa79028-8fe0-4b68-abd1-b04f54b0dd8e';
+const baseUrl = 'https://project-1-api.herokuapp.com'
 
-const ApiKey = axios.get(`${baseUrl}/register`)
+
+
+axios.get(`${baseUrl}/register`)
     .then ((response) => {
         console.log(response);
         console.log(response.data.api_key);
@@ -8,16 +11,47 @@ const ApiKey = axios.get(`${baseUrl}/register`)
     }
 );
 
-const apiKeyUrl = `?api_key=${ApiKey}`;
-console.log(apiKeyUrl);
-
 class BandSiteApi {
-    constructor (apiKey) {
+    constructor (apiKey) {         
         this.apiKey = apiKey;
         this.baseUrl = baseUrl;
     }
-
+    getComment() {
+        return axios.get(this.baseUrl + '/comments?api_key=' + this.apiKey)
+            .then ((response) => 
+                (response)
+            )
+    }
+    getShows() {
+        return axios.get(this.baseUrl + '/showdates?api_key=' + this.apiKey)
+        .then ((response) => 
+            (response)
+        )
+    }
+    postComment() {
+        return axios.post(this.baseUrl + '/comments?api_key=' + this.apiKey)
+        .then ((response) => 
+            (response)
+        )
+    }
 };
+
+const bandSiteApi = new BandSiteApi (apiKey);
+    bandSiteApi.getComment()
+    .then ((response) => {
+        console.log(response.data)
+    })
+    bandSiteApi.getShows()
+    .then ((response) => {
+        console.log(response.data)
+    })
+
+
+
+
+
+
+    // google MDN docs defining method withinj JS class (syntax)
 
 
 
